6d0173ce5978fbde82e1f14728f40c889a206dad
Charlotte Weber <Charlotte@Charlottes-Air.lan>
2018-08-25 23:50:13 +0200

    add config file

---
+++ R/config.R
@@ -0,0 +1,19 @@
+# pick the ones with voting rights (class a), except for brk b
+# FB, COTY (only a)
+# GOOGL (c: GOOG), under armour (c: UA), NWSA (b: NWS)
+
+# brighthouse financial (BHF) weird incline 10-17 /7/2017?
+# > On August 4, 2017, Brighthouse Financial completed its separation from 
+# MetLife and began trading on the Nasdaq stock exchange on August 7
+
+tickers <- c("^GSPC", "^N225", "^FTLC", 
+             "NWSA", "UAA", "DISCA","PWR", #BHF
+             "SCG", "MAT", "SRCL", "HRB", "COTY", "EVHC", 
+             "AAPL", "MSFT", "AMZN", "FB",  "BRK-B",
+             "JPM", "GOOGL", "JNJ", "XOM", "BAC")
+
+tickernames <- c("GSPC", "N225", "FTLC", 
+                 "NWSA", "UAA", "DISCA","PWR", "SCG", 
+                 "MAT", "SRCL", "HRB", "COTY", "EVHC", 
+                 "AAPL", "MSFT", "AMZN", "FB",  "BRK",
+                 "JPM", "GOOGL", "JNJ", "XOM", "BAC")
