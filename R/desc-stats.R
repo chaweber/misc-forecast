e10909008ab5e387c76fc741dd3ac489f9c7ecb8
Charlotte Weber <Charlotte@Charlottes-Air.lan>
2018-08-31 21:52:09 +0200

    write to function

--- R/desc-stats.R
+++ R/desc-stats.R
@@ -1,133 +1,102 @@
 library(corrr)
-library(ggplot2)
-library(wesanderson)
-library(vistime)
+library(purrr)
 library(tseries)
+library(stats)
 
-source("R/fetch-data.R")
 source("R/helpers.R")
-source("R/config.R")
+source("R/helpers-transform.R")
 
-# desc. stats
+# function to get desc stats 
+# by takes in either "prices" or "returns"
 
-# read data # ----
-data <- fetch_data(tickers = tickers, tickernames = tickernames)
+desc_stats <- function(data, by = "log_return", stocktypes = c("index", "bottom")){
+  
+  ifelse((by == "log_returns"), name <- "Logartihmic Returns", name <- "Adjusted Closing Prices")
+  
+  period <- monthly_stocks$date[1:2] %>% diff.Date() %>% as.integer()
+  ifelse((period > 5), period <- "monthly", period <- "daily")
+  
+  
+  # (1) correlations between prices/returns of diff. assets, incl. corr heatmap ----
+  
+  # get correlations
+  corr <- get_corr(stockdata = data, 
+                   stocktypes = stocktypes,
+                   variable = by)
+  
+  # plot correlation heatmap and save to file
+  
+  jpeg(filename = paste0("heatmap_", period, "_", by, ".jpg"), width = 1500, units = "px")
+  rplot(corr, shape = 20) +
+    geom_point() + 
+    labs(title = paste("Correlations between", period, name, "of Assets"), 
+       subtitle = paste("Indices and S&P 500 Companies"))
+  dev.off()
 
-daily_stocks <- data$daily_stocks
-monthly_stocks <- data$monthly_stocks
-
-monthly_stocks %>%
-  filter_time(time_formula = "2016-01-01" ~ "end") %>%
-  vistime(., events = "symbol", groups = "stocktype", start = "date", end = "date")
-
-# daily_stocks %>%
-#   ggplot() + 
-#   geom_segment(aes(x=Begin, xend=End, y=EventID, yend=EventID, group=Sep), size=12)
-
-# correlations between prices / returns of diff. assets / indices (incl. corr heatmap) # ----
-
-# get correlations
-data_corr <- daily_stocks %>%
-  filter(stocktype %in% c("index", "top")) %>%
-  select(symbol, date, adjusted) %>%
-  spread(symbol, adjusted) %>%
-  drop_col(., name = "date") %>%
-  correlate(use = "pairwise.complete.obs") %>%    
-  rearrange(method = "HC", absolute = FALSE) %>%  
-  shave() 
-
-# plot correlation heatmap
-rplot(data_corr) +
-  labs(title = "Correlations between Adjusted Closing Prices", subtitle = "Indices and S&P 500 Largest Companies")
-
-# summary of distribution of returns # ----
-day_summary <- daily_stocks %>%
-  group_by(symbol) %>%
-  summarise(return_av = mean(log_return),
-            return_var = var(log_return),
-            return_skew = skewness(log_return),
-            return_kurt = kurtosis(log_return))
-
-month_summary <- monthly_stocks %>%
-  group_by(symbol) %>%
-  summarise(return_av = mean(log_return),
-            return_var = var(log_return),
-            return_skew = skewness(log_return),
-            return_kurt = kurtosis(log_return))
-
-
-# test for normality of returns # ----
-
-jarque_bera(stockdata = daily_stocks, time_period = "daily")
-jarque_bera(stockdata = monthly_stocks, time_period = "monthly")
-
-# plot distribution of returns (histogram / density curve / normal qq-plot) ----
-
-# hist 
-
-stocks_month %>%
-  filter(stocktype == "bottom") %>%
-  group_by(symbol) %>%
-  ggplot(., aes(x=log_return, fill = symbol)) +
-  geom_histogram(aes(y=..density..), binwidth = 0.003, alpha = 0.8) +
-  scale_color_manual(values = wes_palette(n=10, name="Rushmore1", type = "continuous")) +
-  facet_grid(symbol ~ .) +
-  coord_cartesian(xlim = c(-0.1, 0.1)) +
-  labs(x = "Logarithmic Returns", 
-       y = "Density", 
-       title = "Distribution of Monthly Returns", 
-       subtitle = "Smallest S&P 500 Companies")
-
-# density
-
-stocks %>%
-  filter(stocktype == "index") %>%
-  ggplot(., aes(x=log_return, colour = symbol, fill = symbol)) +
-  stat_density(aes(y=..density..), geom = "line", alpha = 0.8) +
-  scale_color_manual(values = wes_palette(n=3, name="Rushmore1")) +
-  coord_cartesian(xlim = c(-0.1, 0.1)) +
-  labs(x = "Logarithmic Returns", y = "Density", title = "Distribution of Log Returns for Indices")
-
-# normal-qq plot 
-stocks %>%
-  filter(stocktype == "bottom") %>%
-  ggplot(aes(sample = log_return, colour = symbol)) +
-  stat_qq(size = 0.5) +
-  scale_color_manual(values = wes_palette(n=10, name="FantasticFox1", type = "continuous")) +
-  facet_wrap(symbol ~ .) +
-  labs(x = "Theoretical Quantiles", 
-       y = "Sample Quantiles", 
-       title = "Normal QQ Plot",
-       subtitle = "Daily Logarithmic Returns")
-
-
-
-
-# normalise adjusted value for given period ----
-
-calc_period <- c("2014-01-01", "end")
-
-data <- stocks %>% normalise_by_first(calc_period = calc_period, 
-                                      ticker = tickernames,
-                                      stockdata = .) %>%
-  normalise_by_mean(calc_period = calc_period, 
-                    ticker = tickernames,
-                    stockdata = .)
-
-# plot charts ----
-
-# facet grid prices 
-data %>%
-  ggplot(., aes(x = date, y = first_norm, color = symbol)) +
-  geom_line(size = 0.5) +
-  scale_color_manual(values = wes_palette(n=23, name="Rushmore1", type = "continuous")) +
-  facet_grid(stocktype ~.) +
-  labs(title = "Price Movement largest vs. smallest S&P 500 Companies", 
-       y = "Normalised Prices", 
-       x = "Date",
-       caption = "(based on daily adjusted closing prices, normalised resp. 01-01-2014)")
-
-
-# index against top / bottom
-
-print(plot_base(ticker = c("NWSA", "UAA", "DISCA")))
+  
+  # (2) investigate distribution of returns ----
+  
+  # summary of distribution parameters by asset
+  summary <- data %>%
+    group_by(symbol) %>%
+    summarise(aver = mean(eval(as.name(by))),
+              var = var(eval(as.name(by))),
+              skew = skewness(eval(as.name(by))),
+              kurt = kurtosis(eval(as.name(by))))
+  
+  # test for normality
+  jb <- test_stats(FUN = jarque.bera.test,
+             fun_name = "JB",
+             stockdata = data,
+             by = by,
+             text = name)
+  print(jb)
+  
+  
+  # (3) investigate stationarity ----
+  
+  box <- test_stats(FUN = Box.test,
+             fun_name = "LB",
+             stockdata = data,
+             by = by,
+             text = name)
+  print(box)
+  
+  adf <- test_stats(FUN = adf.test,
+             fun_name = "ADF",
+             stockdata = data,
+             by = by,
+             text = name)
+  print(adf)
+  
+  # get acf
+  data_acf <- get_acf(data, by = by)
+  
+  #### check again -------
+  # plot acf
+  jpeg(filename = paste0("acf_", period, "_", by, ".jpg"), width = 1500, units = "px")
+  
+  acf_daily_returns %>%
+    filter(symbol == "GSPC") %>%
+    ggplot(aes(x = lag, y = acf_values, color = symbol, group = symbol)) +
+    theme_bw() +
+    geom_hline(yintercept = 0) +
+    geom_line(aes(y = cutoff_upper), color = "blue", linetype = 2) +
+    geom_line(aes(y = cutoff_lower), color = "blue", linetype = 2) +
+    geom_segment(aes(xend = lag, yend = 0), size = 0.8) +
+    facet_wrap(~symbol, ncol = 3) +
+    expand_limits(y = c(-1, 1)) +
+    labs(title = "ACF Plot", x = "Lag") +
+    theme(legend.position = "none")
+  
+  dev.off()
+  
+  # return results ---- 
+  return(list(correlations = corr,
+              distribution = summary,
+              acf_function = data_acf,
+              jb = jb,
+              adf = adf,
+              lb = box))
+  
+}
