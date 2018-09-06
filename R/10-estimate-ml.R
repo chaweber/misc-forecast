0322769e3619488a2115b74d39d1dbb9bced9f1b
Charlotte Weber <Charlotte@Charlottes-Air.lan>
2018-09-06 06:16:11 +0200

    ml estimation in function

--- R/10-estimate-ml.R
+++ R/10-estimate-ml.R
@@ -1,110 +1,121 @@
-library(h2o)
+# for log return
 
-source("R/01-config.R")
-source("R/machine-learning/01-prepare-ml.R")
-
-# read data ----
-
-lags <- 1:30
-data <- prepare_data(tickers = tickers, 
-                     tickernames = tickernames, 
-                     startdate = startdate, 
-                     enddate = enddate,
-                     lags = lags,
-                     test = 0.1,
-                     by = "adjusted")
-
-# init h2o ----
-h2o.init(ip = "localhost", min_mem_size = "2G", max_mem_size = "5G")
-
-train <- data  %>% filter(group == "train") %>% as.h2o()
-test <- data  %>% filter(group == "test") %>% as.h2o()
-
-if (nrow(data  %>% filter(group == "valid")) > 0){
-  valid <- data  %>% filter(group == "valid") %>% as.h2o()
+estimate_ml <- function(stockdata, lags, by){
+  
+  # read data ----
+  data <- prepare_ml_data(stockdata, 
+                          tickernames = tickernames, 
+                          startdate = startdate, 
+                          enddate = enddate,
+                          lags = lags,
+                          train = 0.8,
+                          test = 0.1,
+                          by = by)
+  
+  for(ticker in tickernames){
+    
+    # init h2o ----
+    h2o::h2o.init(ip = "localhost", min_mem_size = "2G", max_mem_size = "5G")
+    
+    train <- data  %>% filter(symbol = ticker) %>% filter(group == "train") %>% h2o::as.h2o()
+    test <- data %>% filter(symbol = ticker) %>% filter(group == "test") %>% h2o::as.h2o()
+    
+    if (nrow(data  %>% filter(group == "valid")) > 0){
+      valid <- data  %>% filter(symbol = ticker) %>% filter(group == "valid") %>% h2o::as.h2o()
+    }
+    
+    # fix training parameters ----
+    params <- list(forest = list(ntrees = c(20, 80), 
+                                 max_depth = c(5, 50)),
+                   boost = list(ntrees = c(20, 80), 
+                                max_depth = c(2, 20), 
+                                learn_rate = c(0.01, 0.3)))
+    
+    search_criteria <- list(strategy = "RandomDiscrete", 
+                            max_runtime_secs = 900, 
+                            stopping_metric = "MSE", 
+                            stopping_tolerance = 0.001, 
+                            stopping_rounds = 6, 
+                            seed = 40)
+    
+    # set variables
+    y <- by
+    x <- paste0("lag_", lags)
+    
+    # perform grid search----
+    
+    # Regression Forest
+    rf <- h2o::h2o.grid("randomForest",
+                        y = y, 
+                        x = x, 
+                        training_frame = train,
+                        validation_frame = valid, 
+                        hyper_params = params$forest,
+                        search_criteria = search_criteria,
+                        grid_id = "rf")
+    
+    # GBM
+    gbm <- h2o::h2o.grid("gbm",
+                         y = y, 
+                         x = x, 
+                         training_frame = train,
+                         validation_frame = valid, 
+                         hyper_params = params$gbm,
+                         search_criteria = search_criteria,
+                         grid_id = "gbm")
+    
+    # get best model of each grid ----
+    rf_grid <- h2o::h2o.getGrid(grid_id = "rf", sort_by = "RMSE", decreasing = TRUE)
+    rf_best <- h2o::h2o.getModel(rf_grid@model_ids[[1]])
+    
+    cat(title = paste("RF of", ticker),
+        capture.output(summary(rf_best)), 
+        file=paste0("rf_", ticker, ".txt"), 
+        sep="n", 
+        append = TRUE)
+    
+    gbm_grid <- h2o::h2o.getGrid(grid_id = "gbm", sort_by = "RMSE", decreasing = TRUE)
+    gbm_best <- h2o::h2o.getModel(gbm_grid@model_ids[[1]])
+    
+    cat(title = paste("RF of", ticker),
+        capture.output(summary(rf_best)), 
+        file=paste0("rf_", ticker, ".txt"), 
+        sep="n", 
+        append = TRUE)
+    
+    # investigate performance of best model on test set ----
+    
+    rf_performance <- h2o::h2o.performance(model = rf_best, newdata = test)
+    gbm_performance <- h2o::h2o.performance(model = gbm_best, newdata = test)
+    
+    # Get Predictions on Test Set and merge to Data
+    
+    rf_predictions <- h2o::h2o.predict(rf_best, test) %>% as.vector
+    gbm_predictions <- h2o::h2o.predict(gbm_best, test) %>% as.vector
+    
+    test_predict <- data %>%
+      filter(symbol = ticker) %>%
+      filter(group == "test") %>% 
+      select(symbol, group, stocktype, date, log_return) %>%
+      add_column(., "prediction_gbm" := gbm_predictions) %>%
+      add_column(., "prediction_rf" := rf_predictions)
+    
+    accuracy <- forecast::accuracy(f = as.vector(test_predict$prediction_rf), 
+                                   x = test_predict %>% select(log_return))
+    
+    data_predict <- data %>%
+      filter(symbol = ticker) %>%
+      filter(group %in% c("train", "valid")) %>% 
+      select(symbol, group, stocktype, date, log_return) %>% 
+      add_column(., "prediction_gbm" := as.numeric(NA)) %>%
+      add_column(., "prediction_rf" := as.numeric(NA))
+    
+    data_predict <- union(data_predict, test_predict) %>%
+      as_tbl_time(index = date) %>%
+      arrange(date) %>%
+      group_by(symbol)
+    
+    
+  }
+  
 }
-
-# fix training parameters ----
-params <- list(forest = list(ntrees = c(20, 80), 
-                             max_depth = c(5, 50)),
-               boost = list(ntrees = c(20, 80), 
-                            max_depth = c(2, 20), 
-                            learn_rate = c(0.01, 0.3)))
-
-search_criteria <- list(strategy = "RandomDiscrete", 
-                        max_runtime_secs = 900, 
-                        stopping_metric = "MSE", 
-                        stopping_tolerance = 0.001, 
-                        stopping_rounds = 6, 
-                        seed = 40)
-
-# 1-day forecast, based on lagged values ----
-
-# set variables
-y <- "adjusted"
-x <- paste0("lag_", lags)
-
-# perform grid search----
-
-#Regression Forest
-rf <- h2o.grid("randomForest",
-               y = y, 
-               x = x, 
-               training_frame = train,
-               validation_frame = valid, 
-               hyper_params = params$forest,
-               search_criteria = search_criteria,
-               grid_id = "rf")
-
-# GBM
-gbm <- h2o.grid("gbm",
-               y = y, 
-               x = x, 
-               training_frame = train,
-               validation_frame = valid, 
-               hyper_params = params$gbm,
-               search_criteria = search_criteria,
-               grid_id = "gbm")
-
-# glm <- h2o.automl("glm",
-#                 y = y, 
-#                 x = x,
-#                 family = "gaussian",
-#                nfolds = 0,
-#                 training_frame = train,
-#                 validation_frame = valid, 
-#                 model_id = "glm",
-#                seed = 334)
-
-
-
-# get best model of each grid and investigate performance statistics on test set ----
-rf_grid <- h2o.getGrid(grid_id = "rf", sort_by = "RMSE", decreasing = TRUE)
-rf_best <- h2o.getModel(rf_grid@model_ids[[1]])
-rf_performance <- h2o.performance(model = rf_best, newdata = test)
-
-gbm_grid <- h2o.getGrid(grid_id = "gbm", sort_by = "RMSE", decreasing = TRUE)
-gbm_best <- h2o.getModel(gbm_grid@model_ids[[1]])
-gbm_performance <- h2o.performance(model = gbm_best, newdata = test)
-
-
-# Get Predictions on Test Set 
-test_predict <- data  %>% 
-  filter(group == "test") %>% 
-  select(symbol, group, stocktype, date, adjusted) 
-
-gbm_predictions <- h2o.predict(gbm_best, test) %>% as.vector
-
-test_predict <- test_predict %>%
-  add_column(., "prediction_gbm" := gbm_predictions)
-
-data_predict <- data %>%
-  filter(group %in% c("train", "valid")) %>% 
-  select(symbol, group, stocktype, date, adjusted) %>% 
-  add_column(., "prediction_gbm" := as.numeric(NA))
-
-data_predict <- union(data_predict, test_predict) %>%
-  as_tbl_time(index = date) %>%
-  arrange(date) %>%
-  group_by(symbol)
-
